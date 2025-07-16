f321326c9dcfc541b56a486812c0d713f80a8905
Janos Molnar <standby929@gmail.com>
2025-07-16 17:39:16 +0200

    chore: use env variable for backend url instead of hardcoded value

--- admin-frontend/vite.config.ts
+++ admin-frontend/vite.config.ts
@@ -1,20 +1,26 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss()
-  ],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:5000', // Express backend is running on port 5000
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
-})
+export default defineConfig(({ mode }) => {
+  // load the environment variables based on the mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  if (!env.VITE_API_BASE_URL) {
+    throw new Error('VITE_API_BASE_URL is not defined in the environment variables');
+  }
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        }
+      }
+    }
+  };
+});
--- leaderboard/vite.config.ts
+++ leaderboard/vite.config.ts
@@ -1,7 +1,26 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(({ mode }) => {
+  // load the environment variables based on the mode
+  const env = loadEnv(mode, process.cwd(), '');
+
+  if (!env.VITE_API_BASE_URL) {
+    throw new Error('VITE_API_BASE_URL is not defined in the environment variables');
+  }
+  return {
+    plugins: [
+      react(),
+      tailwindcss()
+    ],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_BASE_URL,
+          changeOrigin: true,
+        }
+      }
+    }
+  };
+});
